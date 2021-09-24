95f7cad73a2045c8953e313f2afb7b9935ada334
Williams Alan <alan_lucaswilliams@hotmail.com>
2021-09-24 08:18:14 -0300

    se sube la base de ventas

---
+++ baseVentasAerolinea.sql
@@ -0,0 +1,73 @@
+drop database if exists aerolinea;
+create database aerolinea;
+use aereolinea;
+
+create table Boleto(
+NroBoleto int  auto_increment primary key,
+HoraSalida date,
+FechaViaje date,
+HoraViaje time,
+Monto float,
+IdTipoBoleto int,
+viajaMascota boolean,
+/*IdVuelo int,*/
+constraint  fk_tipoBoleto foreign key(idTipoBoleto) references tipoBoleto(idTipoBoleto)
+/*,constraint  fk_Vuelo foreign key(IdVuelo) references vuelo(IdVuelo)      cuando se haga el marge, se descomenta con el atributo especifico*/
+)engine=innodb;
+
+create table tipoBoleto{
+IdTipoBoleto int auto_increment primary key,
+NombreTipo varchar(40),
+SeleccionAsientos boolean default false,
+IdEquipajeBodega int,
+CantEquipajeBodega int,
+IdEquipajeMano int,
+CantEquipajeMano int,
+CambioPasajes float,
+DevolucionPasaje boolean,
+MillaAerolineaPlus boolean,/*Se pueden acumular las millas */
+constraint fk_EquipajeBodega foreign key(IdEquipajeBodega) references EquipajeBodega(IdEquipajeBodega),
+constraint fk_EquipajeMano foreign key(IdEquipajeMano) references EquipajeMano(IdEquipajeMano)
+}
+create table Mascota{
+IdMascota int auto_increment primary key,
+NroBoleto int,
+Peso float,
+constraint fk_boletoMascota foreign key(NroBoleto) references Boleto(NroBoleto)
+}
+
+create table EquipajeBodega{
+IdEquipajeBodega int auto_increment primary key
+PiezaKilo int,
+MontoExcesoEquipaje int
+}
+
+create table EquipajeMano{
+IdEquipajeMano int auto_increment primary key
+PiezaKilo int,
+MontoExcesoEquipaje int
+}
+
+create table filaAsiento{
+IdAsiento int primary key,
+LetraAsiento varchar(2)/*A B C D E F G H I J*/
+}
+
+
+
+
+/*
+create table TicketFactura{
+nroTF int auto_increment primary key,
+Tipo varchar(2),/* A o B*/
+/*IVA  varchar(30),/*Responsable Inscripto o Consumidor Final*/
+/*importeF float,
+idEmpleado int,/*Este es la foreing key de el empleado*/
+/*fecha date,
+constraint  fk_empleado foreign key(idEmpleado) references Empleado(idEmpleado)
+}Engine=innoDB;
+
+create table datosTFA{
+nroTFA int auto_increment
+}
+*/
